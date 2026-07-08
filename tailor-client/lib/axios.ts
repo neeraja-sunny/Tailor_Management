665997fd556695bb9506e933774f1e37e1c12048
GROUP <groupnnnr@gmail.com>
2026-07-08 17:28:59 +0530

    commit tttt

--- tailor-client/lib/axios.ts
+++ tailor-client/lib/axios.ts
@@ -1,6 +1,9 @@
 import axios, { type AxiosResponse } from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const baseURL = process.env.NEXT_PUBLIC_API_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://tailor-api-cqfd.onrender.com"
+    : "http://localhost:4000");
 
 const api = axios.create({
   baseURL,
