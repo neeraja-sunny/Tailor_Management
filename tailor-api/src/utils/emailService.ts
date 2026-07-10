e9f7882553f2e9b7d1814947929a6c8f59253e6b
GROUP <groupnnnr@gmail.com>
2026-07-10 15:11:13 +0530

    nodemailer

--- tailor-api/src/utils/emailService.ts
+++ tailor-api/src/utils/emailService.ts
@@ -16,8 +16,8 @@ const getTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER!,
         pass: process.env.EMAIL_PASS!,
